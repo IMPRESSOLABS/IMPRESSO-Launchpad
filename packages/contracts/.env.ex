82d8277a73dfa28b6c6835748863b3725ea8fdd4
stark liew <starkliew@gmail.com>
2025-04-03 19:56:08 +0800

    Impresso Moca and deployment

---
+++ packages/contracts/.env.ex
@@ -0,0 +1,10 @@
+METAMASK_PRIVATE_KEY=ca52794a44e1e4be94197f8bbe10b1e3c8052ac6d72cb44f88299ffb519c19d9
+
+SEPOLIA_API_URL=https://worldchain-sepolia.g.alchemy.com/v2/km-8E0N-6sp9_KehMzoqluHLWoRJFTLs
+ETHERSCAN_API_KEY=
+
+ARBITRUM_API_URL=https://api.arbiscan.io/api
+ARBISCAN_API_KEY=your-arbiscan-api-key
+
+ARBITRUM_NOVA_API_URL=arbitrum_nova_rpc_url
+ARMITRUM_NOVASCAN_API_KEY=your-arbinovascan-api-key
